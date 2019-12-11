435d99f940ce565da89af5947721a986a840bf41
James Fraser <jimbob4t@virginmedia.com>
2019-12-11 14:34:59 +0000

    index action and show action

--- students_controller.rb
+++ students_controller.rb
@@ -5,9 +5,21 @@ require_relative('models/house')
 also_reload('./models/*')
 
 # index
+get '/students' do
+  @students = Student.all()
+  erb(:index)
+end
+
+get '/students/new' do
+
+  erb(:new)
+end
 
 # show
-
+get '/students/:id' do
+  @student = Student.find(params[:id])
+  erb(:student_profile)
+end
 # new
 
 # create
