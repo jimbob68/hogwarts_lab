ee963f0c5d5e562073d4f3faeabc86101c76ce00
James Fraser <jimbob4t@virginmedia.com>
2019-12-11 13:34:27 +0000

    setup

---
+++ students_controller.rb
@@ -0,0 +1,19 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('models/student')
+require_relative('models/house')
+also_reload('./models/*')
+
+# index
+
+# show
+
+# new
+
+# create
+
+# edit
+
+# update
+
+# destroy
